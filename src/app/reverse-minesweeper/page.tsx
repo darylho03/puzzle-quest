9719ac2a469fb22fdac30b1790d480b1878d5a9f
darylho03 <darylho03@gmail.com>
2025-09-30 16:28:40 -0700

    Actual Credit

--- src/app/reverse-minesweeper/page.tsx
+++ src/app/reverse-minesweeper/page.tsx
@@ -30,7 +30,7 @@ export default function HomePage() {
                 <p>Inspiration:</p>
                 <ul>
                     <li>The concept of "Reverse Minesweeper" is obviously inspired by the classic game Minesweeper.</li>
-                    <li>The puzzles and mechanics are also inspired by the Baba is You custom levelpack "Sokoban x Minesweeper" by .</li>
+                    <li>The puzzles and mechanics are also inspired by the Baba is You custom levelpack "Sokoban x Minesweeper" by proamateur.</li>
                     <li>I couldn't find the original source, but I was inspired to make this because of Icely Puzzles and their playthrough.</li>
                     <a href="https://www.youtube.com/watch?v=3yNbV9A48Xo&list=PLg5Ta3CWcYX4bPpCMoRRyHeMNyeq9VnY4">
                     <img src="/baba_reverse_minesweeper.png" alt="Icely Puzzles Video Showcase" style={{ width: 200, marginTop: 10 }} />
